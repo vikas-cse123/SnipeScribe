1419e4cf97b83f22e6a8e08e2ff5b8a04128dc83
vikas-cse123 <sagarraothakur2@gmail.com>
2026-02-11 03:01:20 +0530

    Add authentication middleware

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,32 @@
+import Session from "../models/session.model.js";
+import User from "../models/user.model.js";
+export const authenticateUser = async (req, res, next) => {
+  try {
+    console.log(req.cookies);
+    const session = await Session.findById(req.cookies.sid);
+    console.log(session);
+    if (!session) {
+      return res
+        .status(401)
+        .json({
+          success: false,
+          message: "Authentication required. Please log in.",
+        });
+    }
+    const user = await User.findById(session.userId);
+    console.log({ user });
+    if (!user) {
+      return res
+        .status(401)
+        .json({
+          success: false,
+          message: "Authentication required. Please log in.",
+        });
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log(error);
+    next();
+  }
+};
