1e765abc48106536dc42b3cb91bc4f7f7ab3eb7c
vikas-cse123 <sagarraothakur2@gmail.com>
2026-02-11 15:45:11 +0530

    Implement update note controller

--- controller/notes.controller.js
+++ controller/notes.controller.js
@@ -9,9 +9,7 @@ export const createNote = async (req, res) => {
       description,
       userId: req.user._id,
     });
-    return res
-      .status(201)
-      .json({
+    return res.status(201).json({
       success: true,
       message: "Note created successfully.",
       data: { id: note._id },
@@ -36,6 +34,26 @@ export const deleteNote = async (req, res) => {
     return res.status(500).json({
       success: false,
       message: "Failed to delete note. Please try again later.",
+    });
+  }
+};
+
+export const updateNote = async (req, res) => {
+  try {
+    const title = req.body.title ? req.body.title : undefined;
+    const description = req.body.description ? req.body.description : undefined;
+    await Note.updateOne(
+      { _id: req.params.noteId, userId: req.user._id },
+      { title, description },
+    );
+    return res
+      .status(200)
+      .json({ success: true, message: "Note updated successfully." });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Failed to update note. Please try again later.",
     });
   }
 };
--- routes/notes.routes.js
+++ routes/notes.routes.js
@@ -1,11 +1,13 @@
 import express from "express"
-import { createNote, deleteNote } from "../controller/notes.controller.js"
+import { createNote, deleteNote, updateNote } from "../controller/notes.controller.js"
 import { authenticateUser } from "../middlewares/auth.middleware.js"
 import { verifyNoteOwnership } from "../middlewares/verifyNoteOwnership.middleware.js"
 const router = express.Router()
 
 router.post("/",authenticateUser,createNote)
 router.delete("/:noteId",authenticateUser,verifyNoteOwnership,deleteNote)
+router.patch("/:noteId",authenticateUser,verifyNoteOwnership,updateNote)
+
 
 
 export default router
